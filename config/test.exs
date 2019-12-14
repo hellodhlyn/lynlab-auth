d3554501e7022de9ea5add99fc46782adeaba54a
Do Hoerin <hellodhlyn@gmail.com>
2019-12-14 23:18:29 +0900

    Adding workflows

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :luppiter_auth, LuppiterAuth.Repo,
   username: "postgres",
   password: "postgres",
   database: "luppiter_auth_test",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
